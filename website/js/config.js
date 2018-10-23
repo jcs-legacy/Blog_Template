100c3ad28e02f059a3e265373b3db46157a8a45c
jenchieh <jcs090218@gmail.com>
2018-10-23 22:53:58 +0800

    Add js lib and client core.

---
+++ website/js/config.js
@@ -0,0 +1,42 @@
+/**
+ * $File: config.js $
+ * $Date: 2018-09-28 22:33:54 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+// Manual config.
+const blog_name = "[Blog Name]";
+
+const announcement_text = "Welcome to [Blog Name]'s Blog!!!~~~";
+
+// Copyright text to show on the website.
+const copyright_text = "Copyright © [yyyy] [Group Name]. Built: [yyyy-mm-dd].";
+
+// Default content page.
+const intro_content = 'intro';
+// Search page.
+const search_content = 'search';
+
+// Homepage Link
+const homepage_url = "https://github.com/jcs090218/Blog-Template";  // The actual link to your homepage.
+const homepage_text = "blog-tem.github";  // Text to show on the browser
+
+
+// Content extension to load.
+//
+// NOTE(jenchieh): have to be the same with 'server' side's
+// config file.
+//
+// SEE : confid.js in the project root directory.
+const content_extension = '.html';
+
+// Search Input config
+const si_blog_placeholder = "Search blog...";
+
+const si_input_size = 17;  // character unit.
